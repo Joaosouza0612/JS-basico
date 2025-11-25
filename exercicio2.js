fbe1af2a96d2ca1c9592e317373f99b5ea045998
ViniciusPereiraBardini1 <viniciuspereirabardini1@gmail.com>
2025-11-25 09:48:10 -0300

    exercicio2

---
+++ exercicio2.js
@@ -0,0 +1,14 @@
+function somarPares(arr) {
+    let soma = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] % 2 === 0) {
+            soma += arr[i];
+        }
+    }
+
+    return soma;
+}
+
+const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log(somarPares(numeros));
