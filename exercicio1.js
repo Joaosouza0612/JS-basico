d5c35873ce3ccfa39a0dc094b200e5f7ccd4af23
LzzDusCompiuter <luiz.fer240708@gmail.com>
2025-11-28 08:31:13 -0300

    Faz com que ele escolha o maior número

--- exercicio1.js
+++ exercicio1.js
@@ -3,3 +3,13 @@ function encontrarMaiorNumero(numeros) {
   if (numeros.length === 0) {
     return "Array vazio";
   }
+  let maior = numeros[0];
+
+  for (let i = 1; i < numeros.length; i++) {
+    if (numeros[i] > maior) {
+      maior = numeros[i];
+    }
+  }
+
+  return maior;
+}
