329176d559b1bd90a644be850f51cb1af8d70e43
LzzDusCompiuter <luiz.fer240708@gmail.com>
2025-11-28 09:38:12 -0300

    Exemplo de uso exercicio 4

--- exercicio4.js
+++ exercicio4.js
@@ -2,7 +2,7 @@ function contarVogais(texto) {
   let contador = 0;
 
   for (let i = 0; i < texto.length; i++) {
-    let caractere = texto[i].toLowerCase(); // garante que maiúsculas também contem
+    let caractere = texto[i].toLowerCase();
 
     if (
       caractere === "a" ||
@@ -17,3 +17,5 @@ function contarVogais(texto) {
 
   return contador;
 }
+const texto = "Hello World! Programando em JavaScript.";
+console.log(contarVogais(texto)); // 11
