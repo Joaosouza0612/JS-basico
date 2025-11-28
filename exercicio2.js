4d60b1429d164c7a414ffc149fea233d2eb4ce30
juliacaroline12 <jn149575@gmail.com>
2025-11-28 08:16:54 -0300

    exercicio2resolvido

---
+++ exercicio2.js
@@ -0,0 +1,14 @@
+function somarPares(array) {
+    let soma = 0;
+
+for (let i = 0; i < array.length; i++) {
+    if (array[i] % 2 === 0) {  // verifica se é par
+         soma += array[i];     // soma os pares
+       }
+    }
+
+    return soma;
+}
+
+const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log(somarPares(numeros))
