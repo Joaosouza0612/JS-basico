9ba90d9e8af9de7f08cfacb3a9d8af8d8ace8f6d
LzzDusCompiuter <luiz.fer240708@gmail.com>
2025-11-28 09:36:16 -0300

    Exemplo de uso exercicio 5

--- exercicio5.js
+++ exercicio5.js
@@ -1,16 +1,25 @@
 function encontrarProdutoMaisCaro(produtos) {
-  // Se o array estiver vazio
+
   if (produtos.length === 0) {
     return "Nenhum produto encontrado";
   }
 
-  let maisCaro = produtos[0]; // começa assumindo que o primeiro é o mais caro
+  let maisCaro = produtos[0];
 
   for (let i = 1; i < produtos.length; i++) {
     if (produtos[i].preco > maisCaro.preco) {
-      maisCaro = produtos[i]; // atualiza se encontrar produto mais caro
+      maisCaro = produtos[i];
     }
   }
 
   return maisCaro;
 }
+const produtos = [
+  { nome: "Teclado Mecânico", preco: 350 },
+  { nome: "Monitor Ultrawide", preco: 1500 },
+  { nome: "Mouse Gamer", preco: 120 },
+  { nome: "Webcam Full HD", preco: 200 }
+];
+
+console.log(encontrarProdutoMaisCaro(produtos));
+
