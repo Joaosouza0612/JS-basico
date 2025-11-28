3eb209caf3f2ac05b1fa4f1ad3ca4079c56baf5a
Ordep34 <pedro.acacio.cunha2@gmail.com>
2025-11-28 09:41:50 -0300

    exercicio 5

---
+++ exercicio5.js
@@ -0,0 +1,24 @@
+function encontrarProdutoMaisCaro(produtos) {
+    if (produtos.length === 0) return null;
+
+    let produtoMaisCaro = produtos[0];
+
+    for (let i = 1; i < produtos.length; i++) {
+        if (produtos[i].preco > produtoMaisCaro.preco) {
+            produtoMaisCaro = produtos[i];
+        }
+    }
+
+    return produtoMaisCaro;
+}
+
+
+const produtos = [
+    { nome: "Teclado Mecânico", preco: 350 },
+    { nome: "Monitor Ultrawide", preco: 1500 },
+    { nome: "Mouse Gamer", preco: 120 },
+    { nome: "Webcam Full HD", preco: 200 }
+];
+
+console.log(encontrarProdutoMaisCaro(produtos));
+
