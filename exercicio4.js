0783d6b4bd3e5caaba69edea70aa0d59923cd339
juliacaroline12 <jn149575@gmail.com>
2025-11-28 08:22:09 -0300

    exercicio4 resolvido

---
+++ exercicio4.js
@@ -0,0 +1,15 @@
+function contarVogais(texto) {
+    const vogais = "aeiouAEIOU";
+    let contador = 0;
+
+ for (let i = 0; i < texto.length; i++) {
+    if (vogais.includes(texto[i])) {
+            contador++;
+        }
+    }
+
+    return contador;
+}
+
+const texto = "Hello World! Programando em JavaScript.";
+console.log(contarVogais(texto))
