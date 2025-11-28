9ea3c4c1463e3cca0652275edda55d0cc6c6e28f
LzzDusCompiuter <luiz.fer240708@gmail.com>
2025-11-28 08:37:35 -0300

    Exercicio3 Verificar status aluno

---
+++ exercicio3.js
@@ -0,0 +1,13 @@
+function verificarStatusAluno(aluno) {
+  let status = "";
+
+  if (aluno.nota >= 7) {
+    status = "Aprovado";
+  } else if (aluno.nota >= 5) {
+    status = "Recuperação";
+  } else {
+    status = "Reprovado";
+  }
+
+  return `${aluno.nome:} ${status}`;
+}
