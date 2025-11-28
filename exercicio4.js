c8641c167c8bc524ca7a993690afbd22b0ec8081
andrejunior08 <andrejrlopes09@gmail.com>
2025-11-28 09:45:32 -0300

    resolução ex4

---
+++ exercicio4.js
@@ -0,0 +1,17 @@
+function contarVogais(texto) {
+  let contador = 0; 
+  const vogais = "aeiouAEIOU"; 
+
+  for (let i = 0; i < texto.length; i++) {
+    if (vogais.includes(texto[i])) { 
+      contador++;
+    }
+  }
+
+  return contador;
+}
+
+const texto = "Hello World! Programando em JavaScript.";
+const resultado = contarVogais(texto);
+
+console.log("Total de vogais:", resultado);
