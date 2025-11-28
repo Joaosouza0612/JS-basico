563ce4fce8a9b8e6928ff8196c0a69fbc82bb13a
nogueira-h <nogueira.info09@gmail.com>
2025-11-28 08:21:23 -0300

    exercicio 1(ju me ajudou)

--- exercicio1.js
+++ exercicio1.js
@@ -1,1 +1,13 @@
-//aa
+function maiorn(n) {
+  if (n.length === 0) return;
+  let m = n[0]; 
+  
+  for (let i = 1; i < n.length; i++) {
+    if (n[i] > m) m = n[i];
+  }
+  
+  return m;
+}
+const numeros = [15, 8, 23, 4, 42, 16];
+console.log(maiorn(numeros)); 
+console.log(maiorn([]))  
