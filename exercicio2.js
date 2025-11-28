7510c00f18b67f29b0c7bf452392898d74a4dd4f
andrejunior08 <andrejrlopes09@gmail.com>
2025-11-28 09:42:25 -0300

    resolução do ex2

---
+++ exercicio2.js
@@ -0,0 +1,17 @@
+function somarPares(array) {
+  let soma = 0; 
+
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] % 2 === 0) { 
+      soma += array[i]; 
+    }
+  }
+
+  return soma;
+}
+
+const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const resultado = somarPares(numeros);
+
+console.log("Soma dos números pares:", resultado);
+
