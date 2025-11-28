a253a23c6a5e4fcdf7578310c05804d3b2fc4833
Gabriel <bielsilcardoso166@gmail.com>
2025-11-28 08:30:59 -0300

    Exercicio 1 completo

--- exercicio1.js
+++ exercicio1.js
@@ -4,3 +4,15 @@ function encontrarMaiorNumero(array) {
   }
 
   let maior = array[0];
+
+  for (let i = 1; i < array.length; i++) {
+    if (array[i] > maior) {
+      maior = array[i];
+    }
+  }
+
+  return maior;
+}
+
+const numeros = [15, 8, 23, 4, 42, 16];
+console.log(encontrarMaiorNumero(numeros));
