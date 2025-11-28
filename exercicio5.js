fa13dd6eb63eedbf3d203cede144df692fae0709
vie2000 <viniciuseduardofranca70@gmail.com>
2025-11-28 10:37:55 -0300

    exer5

--- exercicio5.js
+++ exercicio5.js
@@ -1,1 +1,15 @@
-function encontrarProdutoMaisCaro
+function encontrarProdutoMaisCaro(produto) {
+    let produtoMaisCaro = produto[0];
+    for (let i = 0; i < produto.length; i++) {
+        if (produto[i].preco > produtoMaisCaro.preco) {
+            produtoMaisCaro = produto[i];
+        }
+    }
+    return produtoMaisCaro;
+}
+const produtos = [
+    { nome: "Teclado Mecânico", preco: 350 },
+    { nome: "Monitor Ultrawide", preco: 1500 },
+    { nome: "Mouse Gamer", preco: 120 },
+    { nome: "Webcam Full HD", preco: 200 }];
+console.log(encontrarProdutoMaisCaro(produtos));
