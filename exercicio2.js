bcf69696c9168bff90e7082dbb4f7e3d551804ca
vie2000 <viniciuseduardofranca70@gmail.com>
2025-11-28 09:41:47 -0300

    exer2js

--- exercicio2.js
+++ exercicio2.js
@@ -1,3 +1,12 @@
-function somarPares(numeros) {const numero = [22, 4, 3, 8, 20, 10];}
+function somarPares(numeros) {
+    let soma = 0;
     for (let i = 0; i < numeros.length; i++) {
-        i
+        if (numeros[i] % 2 === 0) {
+        soma += numeros[i];
+        }
+    }
+    return soma;
+}
+
+const soma = [10, 6, 4, 12, 20, 8];
+console.log(somarPares(soma));
