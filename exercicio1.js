708c8f4e682a40c8d53a9f1ce817da627ae4bd17
matheuzqy <cedupmatheus4@gmail.com>
2025-12-03 08:33:30 -0300

    exercicio 1 completo

---
+++ exercicio1.js
@@ -0,0 +1,9 @@
+let nome = "João";
+const anoNascimento = 2009;
+let idade = 2025 - anoNascimento;
+let isEstudante = true; 
+
+console.log(`Nome: ${nome} (${typeof nome})`);
+console.log(`Ano de Nascimento: ${anoNascimento} (${typeof anoNascimento})`);
+console.log(`Idade: ${idade} (${typeof idade})`);
+console.log(`É Estudante: ${isEstudante} (${typeof isEstudante})`);
